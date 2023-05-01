ddc436629db31727c47385fcb2414a8e55a71eb7
Liqaing <huotrithy01@gmail.com>
2023-05-01 14:39:37 +0700

    Done check for substring, now fixing url error

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -35,13 +35,31 @@ def search_entry(request):
     if request.method == "POST":
 
         # Get the input from HTML form
-        title = request.POST.get("q")
-
-        # Capitalized title
-        title = str(title).capitalize()
+        title = str(request.POST.get("q"))
 
         # Get entry from encyclopedia
         entry = util.get_entry(title)
+
+        # If title doesn't exist in encycopedia
+        if entry == None:
+
+            # Get all entries name
+            entries_list = util.list_entries()
+
+            # Create new list to store entry that have title as substring
+            new_entries_list = []
+
+            # Check if any entries name has title as its substring
+            for entry in entries_list:
+                if title.lower() in entry.lower():
+                    new_entries_list.append(entry)
+
+            return render(request, "encyclopedia/index.html", {
+                "entries": new_entries_list
+            })
+        
+        # Capitalized title
+        title = title.capitalize()
 
         # Convert markdown file into Html
         entry = markdown.markdown(entry)
